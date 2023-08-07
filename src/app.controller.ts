28268fd640abde7e025e276ea504ec9be5e1995b
John Gomersall <thegoms@gmail.com>
2023-08-07 16:36:45 +0100

    Separate simple count from aggregation

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -130,112 +130,117 @@ export class AppController {
     await client.close();
   }
 
-  @Post('query')
-  async query(@Body() body: any) {
+  @Post('aggregate')
+  async aggregate(@Body() body: any[]) {
     const start = Date.now();
     this.logger.log(body);
 
-    if (Array.isArray(body)) {
-      const match = body.find((o: any) => o['$match'])?.['$match'];
-      const group = body.find((o: any) => o['$group'])?.['$group'];
-      const count = body.some((o: any) => o['$count']);
-      const limit = body.find((o: any) => o['$limit'])?.['$limit'];
-      const skip = body.find((o: any) => o['$skip'])?.['$skip'];
+    const match = body.find((o: any) => o['$match'])?.['$match'];
+    const group = body.find((o: any) => o['$group'])?.['$group'];
+    const count = body.some((o: any) => o['$count']);
+    const limit = body.find((o: any) => o['$limit'])?.['$limit'];
+    const skip = body.find((o: any) => o['$skip'])?.['$skip'];
 
-      let tag = group['_id'].substring(1);
-      if (tag === 'users_tags') tag = 'creator';
+    let tag = group['_id'].substring(1);
+    if (tag === 'users_tags') tag = 'creator';
 
-      const { entity, column } = this.getEntityAndColumn(tag);
-      let qb = this.em.createQueryBuilder(entity, 'pt');
-      if (!count) {
-        qb.select(`${column} _id, count(*) count`);
-      } else {
-        qb.select(`${column}`).distinct();
-      }
-      qb.where('not pt.obsolete');
-
-      const matchTag = Object.keys(match)[0];
-      let matchValue = Object.values(match)[0];
-      const not = matchValue?.['$ne'];
-      if (matchTag) {
-        if (not) {
-          matchValue = not;
-        }
-        const { entity: matchEntity, column: matchColumn } =
-          this.getEntityAndColumn(matchTag);
-        const qbWhere = this.em
-          .createQueryBuilder(matchEntity, 'pt2')
-          .select('*')
-          .where(`pt2.product_id = pt.product_id and pt2.${matchColumn} = ?`, [
-            matchValue,
-          ]);
-        qb.andWhere(`${not ? 'NOT ' : ''}EXISTS (${qbWhere.getKnexQuery()})`);
-      }
-      if (count) {
-        qb = this.em.createQueryBuilder(qb, 'temp');
-        qb.select('count(*) count');
-      } else {
-        qb.groupBy(column).orderBy({ ['2']: 'DESC' });
-        if (limit) qb.limit(limit);
-        if (skip) qb.offset(skip);
-      }
-
-      this.logger.log(qb.getFormattedQuery());
-      const results = await qb.execute();
-      //this.logger.log(results);
-      this.logger.log(
-        `Processed ${tag}${matchTag ? ` where ${matchTag} ${not ? '!=' : '=='} ${matchValue}` : ''
-        } in ${Date.now() - start} ms. Returning ${results.length} records`,
-      );
-      if (count) {
-        const response = {};
-        response[tag] = results[0].count;
-        this.logger.log(response);
-        return response;
-      }
-      return results;
+    const { entity, column } = this.getEntityAndColumn(tag);
+    let qb = this.em.createQueryBuilder(entity, 'pt');
+    if (!count) {
+      qb.select(`${column} _id, count(*) count`);
     } else {
-      const tags = Object.keys(body);
-      const tag = tags[0];
-      const { entity, column } = this.getEntityAndColumn(tag);
-      const qb = this.em.createQueryBuilder(entity, 'pt');
-      qb.select(`count(*) count`);
-      qb.where('not pt.obsolete');
+      qb.select(`${column}`).distinct();
+    }
+    qb.where('not pt.obsolete');
 
-      let matchValue = body[tag];
+    const matchTag = Object.keys(match)[0];
+    let matchValue = Object.values(match)[0];
+    const not = matchValue?.['$ne'];
+    if (matchTag) {
+      if (not) {
+        matchValue = not;
+      }
+      const { entity: matchEntity, column: matchColumn } =
+        this.getEntityAndColumn(matchTag);
+      const qbWhere = this.em
+        .createQueryBuilder(matchEntity, 'pt2')
+        .select('*')
+        .where(`pt2.product_id = pt.product_id and pt2.${matchColumn} = ?`, [
+          matchValue,
+        ]);
+      qb.andWhere(`${not ? 'NOT ' : ''}EXISTS (${qbWhere.getKnexQuery()})`);
+    }
+    if (count) {
+      qb = this.em.createQueryBuilder(qb, 'temp');
+      qb.select('count(*) count');
+    } else {
+      qb.groupBy(column).orderBy({ ['2']: 'DESC' });
+      if (limit) qb.limit(limit);
+      if (skip) qb.offset(skip);
+    }
+
+    this.logger.log(qb.getFormattedQuery());
+    const results = await qb.execute();
+    //this.logger.log(results);
+    this.logger.log(
+      `Processed ${tag}${matchTag ? ` where ${matchTag} ${not ? '!=' : '=='} ${matchValue}` : ''
+      } in ${Date.now() - start} ms. Returning ${results.length} records`,
+    );
+    if (count) {
+      const response = {};
+      response[tag] = results[0].count;
+      this.logger.log(response);
+      return response;
+    }
+    return results;
+  }
+
+  @Post('count')
+  async count(@Body() body: any) {
+    const start = Date.now();
+    this.logger.log(body);
+
+    const tags = Object.keys(body);
+    const tag = tags[0];
+    const { entity, column } = this.getEntityAndColumn(tag);
+    const qb = this.em.createQueryBuilder(entity, 'pt');
+    qb.select(`count(*) count`);
+    qb.where('not pt.obsolete');
+
+    let matchValue = body[tag];
+    const not = matchValue?.['$ne'];
+    if (not) {
+      matchValue = not;
+    }
+    qb.andWhere(`${not ? 'NOT ' : ''}pt.${column} = ?`, [matchValue]);
+    const matchTag = tags[1];
+    let extraMatchLog = '';
+    if (matchTag) {
+      let matchValue = body[matchTag];
       const not = matchValue?.['$ne'];
       if (not) {
         matchValue = not;
       }
-      qb.andWhere(`${not ? 'NOT ' : ''}pt.${column} = ?`, [matchValue]);
-      const matchTag = tags[1];
-      let extraMatchLog = '';
-      if (matchTag) {
-        let matchValue = body[matchTag];
-        const not = matchValue?.['$ne'];
-        if (not) {
-          matchValue = not;
-        }
-        const { entity: matchEntity, column: matchColumn } =
-          this.getEntityAndColumn(matchTag);
-        const qbWhere = this.em
-          .createQueryBuilder(matchEntity, 'pt2')
-          .select('*')
-          .where(`pt2.product_id = pt.product_id and pt2.${matchColumn} = ?`, [
-            matchValue,
-          ]);
-        qb.andWhere(`${not ? 'NOT ' : ''}EXISTS (${qbWhere.getKnexQuery()})`);
-        extraMatchLog += ` and ${matchTag} ${not ? '!=' : '=='} ${matchValue}`;
-      }
-      const results = await qb.execute();
-      //this.logger.log(results);
-      this.logger.log(
-        `Processed ${tag} ${not ? '!=' : '=='} ${matchValue}${extraMatchLog} in ${Date.now() - start} ms.`,
-      );
-      const response = results[0].count;
-      this.logger.log(response);
-      return response;
+      const { entity: matchEntity, column: matchColumn } =
+        this.getEntityAndColumn(matchTag);
+      const qbWhere = this.em
+        .createQueryBuilder(matchEntity, 'pt2')
+        .select('*')
+        .where(`pt2.product_id = pt.product_id and pt2.${matchColumn} = ?`, [
+          matchValue,
+        ]);
+      qb.andWhere(`${not ? 'NOT ' : ''}EXISTS (${qbWhere.getKnexQuery()})`);
+      extraMatchLog += ` and ${matchTag} ${not ? '!=' : '=='} ${matchValue}`;
     }
+    this.logger.log(qb.getFormattedQuery());
+    const results = await qb.execute();
+    //this.logger.log(results);
+    this.logger.log(
+      `Processed ${tag} ${not ? '!=' : '=='} ${matchValue}${extraMatchLog} in ${Date.now() - start} ms.`,
+    );
+    const response = results[0].count;
+    this.logger.log(response);
+    return response;
   }
 
   private getEntityAndColumn(tag: any) {
