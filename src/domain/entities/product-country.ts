58c7b4f66938c52cb496be4935269a31c477771c
John Gomersall <thegoms@gmail.com>
2025-02-15 14:20:09 +0000

    Add product country table
    Signed-off-by: John Gomersall <thegoms@gmail.com>

---
+++ src/domain/entities/product-country.ts
@@ -0,0 +1,23 @@
+import { Entity, Index, ManyToOne, Property } from '@mikro-orm/core';
+import { Product } from './product';
+import { Country } from './country';
+
+@Entity()
+@Index({
+  name: 'product_country_ix1',
+  expression:
+    'create index product_country_ix1 on product_country (country_id, recent_scans DESC, total_scans DESC, product_id)',
+})
+export class ProductCountry {
+  @ManyToOne({ primary: true, onDelete: 'cascade' })
+  product: Product;
+
+  @ManyToOne({ primary: true, onDelete: 'cascade' })
+  country: Country;
+
+  @Property()
+  recentScans: number;
+
+  @Property()
+  totalScans: number;
+}
