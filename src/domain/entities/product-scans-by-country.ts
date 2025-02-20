2aa5e06e0cc66b92cf168dfa8662ed721c61ee76
John Gomersall <thegoms@gmail.com>
2025-02-20 12:13:33 +0000

    Remove unnecessary index
    Signed-off-by: John Gomersall <thegoms@gmail.com>

--- src/domain/entities/product-scans-by-country.ts
+++ src/domain/entities/product-scans-by-country.ts
@@ -1,19 +1,8 @@
-import {
-  Entity,
-  Index,
-  ManyToOne,
-  PrimaryKey,
-  Property,
-} from '@mikro-orm/core';
+import { Entity, ManyToOne, PrimaryKey, Property } from '@mikro-orm/core';
 import { Product } from './product';
 import { Country } from './country';
 
 @Entity()
-@Index({
-  name: 'product_scans_by_country_ix1',
-  expression:
-    'create index product_scans_by_country_ix1 on product_scans_by_country (year, country_id, unique_scans DESC, product_id)',
-})
 export class ProductScansByCountry {
   @ManyToOne({ primary: true, onDelete: 'cascade' })
   product: Product;
