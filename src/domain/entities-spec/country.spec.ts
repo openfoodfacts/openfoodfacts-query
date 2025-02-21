472ae69a3e6b2c9ff08aebe91fec159e07a57fb4
John Gomersall <thegoms@gmail.com>
2025-02-21 18:02:13 +0000

    Fix test
    Signed-off-by: John Gomersall <thegoms@gmail.com>

--- src/domain/entities-spec/country.spec.ts
+++ src/domain/entities-spec/country.spec.ts
@@ -3,7 +3,7 @@ import { addAllCountries } from '../entities/country';
 
 describe('addAllCountries', () => {
   it('should update the codes of existing countries', async () => {
-    await sql`INSERT INTO country (tag) VALUES ('en:france')`;
+    await sql`INSERT INTO country (tag) VALUES ('en:france') ON CONFLICT (tag) DO UPDATE SET code = NULL`;
     await addAllCountries();
     const countries = await sql`SELECT * FROM country`;
     expect(countries.length).toBeGreaterThan(100);
