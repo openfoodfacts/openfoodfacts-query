444a2e8cb6cc0a562ba2237b8978334f6df9f540
John Gomersall <thegoms@gmail.com>
2025-02-26 16:51:57 +0000

    fix: Don't reject world queries
    Signed-off-by: John Gomersall <thegoms@gmail.com>

--- src/domain/services/query.service.ts
+++ src/domain/services/query.service.ts
@@ -279,7 +279,7 @@ export class QueryService {
 
     // TODO: Need to handle multiple countries
     const countryTag = body.filter.countries_tags;
-    if (typeof countryTag !== 'string')
+    if (countryTag !== undefined && typeof countryTag !== 'string')
       this.throwUnprocessableException(
         `Can't support multiple country queries`,
       );
