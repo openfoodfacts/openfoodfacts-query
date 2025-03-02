4e1d8c3ed8b069dd7304fae5784d92d268958b34
John Gomersall <thegoms@gmail.com>
2025-03-02 12:53:38 +0000

    Stick to singular for repository names so match table name
    Signed-off-by: John Gomersall <thegoms@gmail.com>

--- migrations/Migration20230904100447.py
+++ migrations/Migration20230904100447.py
@@ -1,12 +1,12 @@
-import query.repositories.products as products
+import query.repositories.product as product
 import query.repositories.product_tags as product_tags
-import query.repositories.loaded_tags as loaded_tags
-import query.repositories.product_ingredients as product_ingredients
+import query.repositories.loaded_tag as loaded_tag
+import query.repositories.product_ingredient as product_ingredient
 import query.repositories.settings as settings
 
 async def up(connection):
-    await products.create_table(connection)
+    await product.create_table(connection)
     await product_tags.create_tables(connection)
-    await loaded_tags.create_table(connection)
-    await product_ingredients.create_table(connection)
+    await loaded_tag.create_table(connection)
+    await product_ingredient.create_table(connection)
     await settings.create_table(connection)
