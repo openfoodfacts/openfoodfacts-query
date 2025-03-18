4bbf48b5f175f1b1350346fde37367e3b33ebddc
John Gomersall <thegoms@gmail.com>
2025-03-18 15:05:37 +0000

    Multiple messages
    Signed-off-by: John Gomersall <thegoms@gmail.com>

--- query/redis_test.py
+++ query/redis_test.py
@@ -56,23 +56,26 @@ async def test_listener_calls_subscriber_function(
         # Get the most recent message id so we don't pick up old messages
         get_id.return_value = await get_last_message_id(redis, STREAM_NAME)
 
+        # Add some messages
+        product_code = random_code()
+        message_id1 = await add_test_message(redis, product_code)
+        message_id2 = await add_test_message(redis, random_code())
+
         # Start the redis listener
         redis_listener_task = asyncio.create_task(redis_listener())
 
         # Cancel the redis listener as soon as the message is received so we don't have to wait another 5 seconds
         messages_received.side_effect = redis_listener_task.cancel
 
-        # Add a message
-        product_code = random_code()
-        message_id = await add_test_message(redis, product_code)
-
         # TODO: Would like to find a better way to wait for other tasks to process...
         await asyncio.sleep(0.1)
 
-        assert messages_received.called
-        assert set_id.call_args[0][1] == message_id
+        # Settings shou;d be updated with the last message id
+        assert set_id.call_args[0][1] == message_id2
 
         messages = messages_received.call_args[0][0]
-        assert len(messages) == 1
-        assert messages[0][0] == message_id
+        assert len(messages) == 2
+
+        # Messages should be in order
+        assert messages[0][0] == message_id1
         assert messages[0][1]['code'] == product_code
