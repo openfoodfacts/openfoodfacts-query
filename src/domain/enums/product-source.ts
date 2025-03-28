2d6c8bd38320ab68b5844ba6023253dc00ec2b37
John Gomersall <thegoms@gmail.com>
2025-03-28 14:19:22 +0000

    Remove old Typescript project
    Signed-off-by: John Gomersall <thegoms@gmail.com>

--- src/domain/enums/product-source.ts
+++
@@ -1,5 +0,0 @@
-export enum ProductSource {
-  INCREMENTAL_LOAD = 'incremental',
-  FULL_LOAD = 'full',
-  EVENT = 'event',
-}
--- test/global-teardown.ts
+++
@@ -1,6 +0,0 @@
-import sql from '../src/db';
-
-export default async function () {
-  await sql.end();
-  await globalThis.__PGCONTAINER__.stop();
-}
