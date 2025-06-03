34e9e91c7a5b67b7a347ba315faf156daf46e3a1
John Gomersall <thegoms@gmail.com>
2025-06-03 14:56:14 +0100

    Fix typing of response when not delegating to MongoDB Error returned when just finding product codes #128
    Signed-off-by: John Gomersall <thegoms@gmail.com>

--- query/routes_test.py
+++ query/routes_test.py
@@ -1,6 +1,8 @@
 from fastapi import status
 from fastapi.testclient import TestClient
 
+from query.services.query_find_test import create_tags_and_scans
+
 from .database import get_transaction
 from .routes import app
 from .services.query_count_test import create_test_tags
@@ -68,3 +70,22 @@ async def test_aggregate_obsolete():
     assert len(response_data) == 1
     assert response_data[0]["_id"] == tags.origin_value
     assert response_data[0]["count"] == 1
+
+
+async def test_find_just_code():
+    tags = await create_tags_and_scans()
+    client = TestClient(app)
+    response = client.post(
+        "/find",
+        json={
+            "filter": {"origins_tags" : tags.origin_value},
+            "projection": {"code": 1},
+            "sort": [["popularity_key", -1]]
+        },
+    )
+    assert response.status_code == status.HTTP_200_OK
+    results = response.json()
+    assert len(results) == 3
+    assert results[0]["code"] == tags.product2["code"]
+    assert results[1]["code"] == tags.product3["code"]
+    assert results[2]["code"] == tags.product1["code"]
